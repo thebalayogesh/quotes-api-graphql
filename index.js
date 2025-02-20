2a695eb5525ab5b01923fb6eed34cedd42a9175d
thebalayogesh <theyogeshwaran001@gmail.com>
2025-02-20 15:26:32 +0530

    vercecnfig

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import {getAllQuotes, getRandomQuote, addQuote, getAuthorQuotes} from './_db.js';
-import { typeDefs } from './schema.js';
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateLambdaHandler, handlers } from "@as-integrations/vercel";
+import { getAllQuotes, getRandomQuote, addQuote, getAuthorQuotes } from "./_db.js";
+import { typeDefs } from "./schema.js";
 
 const resolvers = {
   Query: {
     quotes: () => getAllQuotes(),
     randomQuote: () => getRandomQuote(),
-    authorQuotes: (_, { author }) => {
-      const result = getAuthorQuotes(author);
-      console.log("Fetched Quotes:", result);
-      return result;
-    },
+    authorQuotes: (_, { author }) => getAuthorQuotes(author),
   },
   Mutation: {
     addQuote: (_, args) => {
@@ -21,19 +17,49 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const server = new ApolloServer({ typeDefs, resolvers });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+export default startServerAndCreateLambdaHandler(server, handlers.createAPIGatewayProxyEventV2Request);
 
-console.log(`Server ready at PORT: ${url}`);
-// console.log(getQuotes());
-// console.log(typeDefs);
 
-// if (!quote || !author || !genre) {
-//         throw new Error("All fields (quote, author, genre) are required.");
-// }
+
+
+// import { ApolloServer } from '@apollo/server';
+// import { startStandaloneServer } from '@apollo/server/standalone';
+// import {getAllQuotes, getRandomQuote, addQuote, getAuthorQuotes} from './_db.js';
+// import { typeDefs } from './schema.js';
+
+// const resolvers = {
+//   Query: {
+//     quotes: () => getAllQuotes(),
+//     randomQuote: () => getRandomQuote(),
+//     authorQuotes: (_, { author }) => {
+//       const result = getAuthorQuotes(author);
+//       console.log("Fetched Quotes:", result);
+//       return result;
+//     },
+//   },
+//   Mutation: {
+//     addQuote: (_, args) => {
+//       const { quote, author, genre } = args;
+//       return addQuote(quote, author, genre);
+//     },
+//   },
+// };
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 4000 },
+// });
+
+// console.log(`Server ready at PORT: ${url}`);
+// // console.log(getQuotes());
+// // console.log(typeDefs);
+
+// // if (!quote || !author || !genre) {
+// //         throw new Error("All fields (quote, author, genre) are required.");
+// // }
